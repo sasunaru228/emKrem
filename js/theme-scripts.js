1af45715e2c3288349f0f9d37a151093c0eea6ea
sasunaru228 <od26102000@gmail.com>
2021-04-10 13:52:29 +0300

    fix darkMode

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -48,9 +48,9 @@ $('.navbar-toggle').click(() => {
 const options = {
   bottom: '64px', // default: '32px'
   right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
+  left: '64px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
+  mixColor: '#FFF', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
